5bf96bc32e4c571be95bf89e90bd0356ad4b6c02
jNetoDev <115748064+jNetoDev@users.noreply.github.com>
2023-03-25 16:49:05 -0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,83 @@
+//Goku ou Vegeta
+// Naruto ou Sassuke
+
+class character {
+    
+    life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+
+    constructor (name) {
+        this.name = name;
+    }
+
+    get life() {
+        return this._life;
+    }
+    set life(newLife) {
+        this._life = newLife <0 ? 0 : newLife;
+    }
+}
+
+class Goku extends character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life;
+    }
+}
+
+class Vegeta extends character {
+    constructor(name) {
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life;
+    }
+}
+
+class Naruto extends character {
+    constructor() {
+        super('Naruto');
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class Sassuke extends character {
+    constructor () {
+        super('Sassuke')
+        this.life = 120;
+        this.attack = 16;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
+
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+      this.fighter1 = fighter1;
+      this.fighter2 = fighter2;
+      this.fighter1El = fighter1El;
+      this.fighter2El = fighter2El;
+    }
+  
+    start() {
+      this.update();
+    }
+  
+    update() {
+      // Fighter 1
+      this.fighter1El.querySelector(".name").innerHTML = this.fighter1.name;
+  
+      // Fighter 2
+      this.fighter2El.querySelector(".name").innerHTML = this.fighter2.name;
+    }
+  }
---
+++ script.js
@@ -0,0 +1,13 @@
+let char = new Goku('Neto');
+let monster = new Naruto ();
+
+
+
+const stage = new Stage(
+    char,
+    monster,
+   document.querySelector('#char'),
+   document.querySelector('#Naruto')
+);
+
+stage.start();
